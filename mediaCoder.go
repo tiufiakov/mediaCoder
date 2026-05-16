61e692024a0d30e79a3ed1ab9949654f5aeec042
ilia <tiufiakovilia255465@gmail.com>
2026-05-16 23:50:42 +0300

    Добавлено подключение к БД и миграция

---
+++ mediaCoder.go
@@ -0,0 +1,21 @@
+package mediaCoder
+
+import (
+	"log"
+
+	"github.com/tiufiakov/mediaCoder/db"
+)
+
+func main() {
+	connstring := "user=postgres password=1234 dbname=mediaCoder host=localhost port=5432 sslmode=disable"
+
+	database, err := db.ConnectToPostgres(connstring)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer database.Close()
+
+	err = db.RunMigrations(database)
+
+}
